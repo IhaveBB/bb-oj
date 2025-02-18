e032f3ac9984616ec64f96a882017ef17546569a
IhaveBB <IhaveBB@outlook.com>
2025-02-18 15:39:59 +0800

    first commit

---
+++ oj-modules/oj-job/src/main/java/com/nicebao/OjJobApplication.java
@@ -0,0 +1,18 @@
+package com.nicebao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * OJSystemApplication
+ * <p>
+ *
+ * @author IhaveBB
+ * @date 2025/2/18
+ */
+@SpringBootApplication
+public class OjJobApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OjJobApplication.class, args);
+	}
+}
