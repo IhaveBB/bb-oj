fc890aafe332f57c73512e96d363cc468145d660
IhaveBB <IhaveBB@outlook.com>
2025-02-20 17:20:03 +0800

    引入nacos 引入网关

---
+++ oj-gateway/src/main/java/com/nicebao/gateway/OjGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.nicebao.gateway;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * OJSystemApplication
+ * <p>
+ *
+ * @author IhaveBB
+ * @date 2025/2/18
+ */
+@SpringBootApplication
+public class OjGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OjGatewayApplication.class, args);
+	}
+}
